9842e7e8ffcbbc30617aec1b29609e8be0e5d366
xX-Mill3r-Xx <millermartinsvasco1@gmail.com>
2022-02-25 20:03:54 -0300

    200325022022

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace CalcularPreco
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_ValorProduto = new System.Windows.Forms.TextBox();
+            this.txt_Quantidade = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_Calcular = new System.Windows.Forms.Button();
+            this.lb_Resultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_ValorProduto
+            // 
+            this.txt_ValorProduto.Font = new System.Drawing.Font("Consolas", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txt_ValorProduto.Location = new System.Drawing.Point(12, 80);
+            this.txt_ValorProduto.Name = "txt_ValorProduto";
+            this.txt_ValorProduto.Size = new System.Drawing.Size(200, 78);
+            this.txt_ValorProduto.TabIndex = 0;
+            this.txt_ValorProduto.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt_Quantidade
+            // 
+            this.txt_Quantidade.Font = new System.Drawing.Font("Consolas", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txt_Quantidade.Location = new System.Drawing.Point(245, 80);
+            this.txt_Quantidade.Name = "txt_Quantidade";
+            this.txt_Quantidade.Size = new System.Drawing.Size(200, 78);
+            this.txt_Quantidade.TabIndex = 1;
+            this.txt_Quantidade.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Valor do Produto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(245, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Quantidade";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(218, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 23);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "X";
+            // 
+            // btn_Calcular
+            // 
+            this.btn_Calcular.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_Calcular.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Calcular.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_Calcular.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_Calcular.ForeColor = System.Drawing.SystemColors.Window;
+            this.btn_Calcular.Location = new System.Drawing.Point(121, 164);
+            this.btn_Calcular.Name = "btn_Calcular";
+            this.btn_Calcular.Size = new System.Drawing.Size(223, 77);
+            this.btn_Calcular.TabIndex = 5;
+            this.btn_Calcular.Text = "Calcular";
+            this.btn_Calcular.UseVisualStyleBackColor = false;
+            this.btn_Calcular.Click += new System.EventHandler(this.btn_Calcular_Click);
+            // 
+            // lb_Resultado
+            // 
+            this.lb_Resultado.Font = new System.Drawing.Font("Consolas", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lb_Resultado.Location = new System.Drawing.Point(12, 249);
+            this.lb_Resultado.Name = "lb_Resultado";
+            this.lb_Resultado.Size = new System.Drawing.Size(435, 207);
+            this.lb_Resultado.TabIndex = 6;
+            this.lb_Resultado.Text = "Resultado";
+            this.lb_Resultado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 464);
+            this.Controls.Add(this.lb_Resultado);
+            this.Controls.Add(this.btn_Calcular);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_Quantidade);
+            this.Controls.Add(this.txt_ValorProduto);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calcular Preços";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_ValorProduto;
+        private System.Windows.Forms.TextBox txt_Quantidade;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_Calcular;
+        private System.Windows.Forms.Label lb_Resultado;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace CalcularPreco
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Calcular_Click(object sender, EventArgs e)
+        {
+            double valor = double.Parse(txt_ValorProduto.Text.ToString(), CultureInfo.InvariantCulture);
+            double quantidade = double.Parse(txt_Quantidade.Text.ToString(), CultureInfo.InvariantCulture);
+            double res = valor * quantidade;
+            btn_Calcular.BackColor = Color.AliceBlue;
+            btn_Calcular.ForeColor = Color.Green;
+
+            //lb_Resultado.ForeColor = Color.Red;
+            //lb_Resultado.Text = "Os campos de valores não podem ser vazios";
+            //txt_ValorProduto.Focus();
+
+            lb_Resultado.Text = $"O produto com valor de {valor} multiplicado pela quantidade de {quantidade} equivale a R${res.ToString("F2", CultureInfo.InvariantCulture)} da moeda Virgente";
+        }
+    }
+}
